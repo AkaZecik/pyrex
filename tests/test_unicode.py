44e4bbd17172324edefd03edee85913c2eceab9f
akazecik <AkaZecik@users.noreply.github.com>
2021-07-18 21:42:21 +0200

    [WIP] Add more methods to rex.pyi

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -2,9 +2,5 @@ import unittest
 
 
 class TestUnicode(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_simple(self) -> None:
+        pass
