aa5dbdeac9699887db3fc8933165e5c4df967853
akazecik <AkaZecik@users.noreply.github.com>
2021-08-18 00:43:22 +0200

    Finish DFA without finishing states

--- dfa.cpp
+++ dfa.cpp
@@ -10,81 +10,68 @@
 #include "nfa.cpp"
 
 struct PowerDFA {
-    struct Node {
-        std::unordered_map<char, Node *> map;
-        // do we perhaps want to collect NFA::Node * instead?
-        std::set<int> ids;
-        char c{};
-
-        explicit Node(int id) : ids({id}) {}
-
-        Node(std::set<int> &&ids, char c) : ids(ids), c(c) {}
+    struct TmpNode {
+        std::set<NFA::Node const *> nodes;
+        std::unordered_map<char, TmpNode *> edges;
 
         struct Compare {
             using is_transparent = void;
 
-            bool operator()(Node *left, Node *right) const {
-                return left->ids < right->ids;
+            bool operator()(TmpNode const *left, TmpNode const *right) const {
+                return left->nodes < right->nodes;
             }
 
-            bool operator()(Node *left, std::set<int> const &right) {
-                return left->ids < right;
+            bool operator()(TmpNode const *left,
+                            std::set<NFA::Node const *> const &right) const {
+                return left->nodes < right;
             }
 
-            bool operator()(std::set<int> const &left, Node *right) {
-                return left < right->ids;
+            bool operator()(std::set<NFA::Node const *> const &left,
+                            TmpNode const *right) const {
+                return left < right->nodes;
             }
         };
     };
 
-    Node start_node{0};
-    std::vector<Node *> end_nodes;
+    TmpNode start_node;
+    std::vector<TmpNode *> end_nodes;
     bool contains_empty = false;
 
     static PowerDFA from_NFA(NFA const &nfa) {
-        PowerDFA dfa;
-        std::set<PowerDFA::Node *, PowerDFA::Node::Compare> dfa_nodes(
-            {&dfa.start_node}
-        );
-        std::set<NFA::Node const *> nfa_nodes({&nfa.start_node});
-        subset_construction(nfa_nodes, &dfa.start_node, dfa_nodes);
+        PowerDFA dfa{
+            .start_node = {.nodes = {&nfa.start_node}},
+            .contains_empty = nfa.contains_empty
+        };
+        std::set<TmpNode *, TmpNode::Compare> all_nodes({&dfa.start_node});
+        subset_construction(&dfa.start_node, all_nodes);
         return dfa;
     }
 
     static void subset_construction(
-        std::set<NFA::Node const *> const &curr_nfa_nodes,
-        PowerDFA::Node const *curr_dfa_node,
-        std::set<PowerDFA::Node *, PowerDFA::Node::Compare> &all_dfa_nodes
+        TmpNode *curr_node, std::set<TmpNode *, TmpNode::Compare> &all_nodes
     ) {
-        // is it ok that type of values is std::set and not std::set *?
+        // make "all nodes of dfa" a vector and just emplace on the back?
+        // and for comparison just store pointers into that vector? (be careful,
+        // vector will change in runtime)
+        // fill in "end_states"
         std::unordered_map<char, std::set<NFA::Node const *>> nfa_edges;
 
-        for (auto nfa_node : curr_nfa_nodes) {
-            for (auto nbh : nfa_node->edges) {
+        for (NFA::Node const *nfa_node : curr_node->nodes) {
+            for (NFA::Node const *nbh : nfa_node->edges) {
                 nfa_edges[nbh->c].insert(nbh);
             }
         }
 
-        std::unordered_map<char, Node *> dfa_edges;
-
         for (auto &[c, nfa_nodes] : nfa_edges) {
-            std::set<int> ids;
+            auto result = all_nodes.find(nfa_nodes);
 
-            for (auto node : nfa_nodes) {
-                ids.insert(node->id);
-            }
-
-            auto result = all_dfa_nodes.find(ids);
-            Node *node = nullptr;
-
-            if (result == all_dfa_nodes.end()) {
-                node = new Node(std::move(set), c);
-                all_dfa_nodes.insert(node);
+            if (result == all_nodes.end()) {
+                auto new_node = new TmpNode{std::move(nfa_nodes), {}};
+                curr_node->edges[c] = new_node;
+                subset_construction(new_node, all_nodes);
             } else {
-                node = *result;
+                curr_node->edges[c] = *result;
             }
-
-            subset_construction(node, all_dfa_nodes);
         }
     }
 };
